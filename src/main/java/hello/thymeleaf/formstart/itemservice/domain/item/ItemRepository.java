1bae2a826a4ce35ec8ea871bd2fb71794844b65b
ChangMinPark2 <98qkrckdals@naver.com>
2024-03-04 16:39:15 +0900

    feat(Spring-MVC2) : itemService - form Start

--- src/main/java/hello/thymeleaf/formstart/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/thymeleaf/formstart/itemservice/domain/item/ItemRepository.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 @Repository
 public class ItemRepository {
-
     private static final Map<Long, Item> store = new HashMap<>(); //static
     private static long sequence = 0L; //static
 
@@ -32,6 +31,10 @@ public class ItemRepository {
         findItem.setItemName(updateParam.getItemName());
         findItem.setPrice(updateParam.getPrice());
         findItem.setQuantity(updateParam.getQuantity());
+        findItem.setOpen(updateParam.getOpen());
+        findItem.setRegions(updateParam.getRegions());
+        findItem.setItemType(updateParam.getItemType());
+        findItem.setDeliveryCode(updateParam.getDeliveryCode());
     }
 
     public void clearStore() {
